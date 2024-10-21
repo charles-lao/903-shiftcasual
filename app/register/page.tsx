48b95b5a8aa2e4bd559d13e084182572b8a92421
Charles Lao <charles.h.lao@gmail.com>
2024-10-21 16:56:16 +1300

    add bg and logo on register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import * as React from "react"
- 
-import { Button } from "@/components/ui/button"
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -10,58 +10,98 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { signup } from "@/lib/auth-actions";
 import { useFormState } from "react-dom";
-
+import Image from "next/image";
 
 export default function RegisterPage() {
-
   const [formState, formAction] = useFormState(signup, {});
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-300">
+    <div
+      className="h-screen flex items-center justify-center"
+      style={{
+        backgroundImage: "url('/images/background.jpg')",
+        backgroundSize: "cover",
+      }}
+    >
       <Card className="w-[350px] p-4 shadow-md">
         <form action={formAction}>
           <CardHeader>
+            <Image
+              src="/images/header.jpg" // Update the path to your image file
+              alt="Website Logo"
+              width={200} // Adjust the width as needed
+              height={200} // Adjust the height as needed
+              unoptimized // Disable image optimization for baseline testing
+              className="mx-auto mb-2" // Centers only the image
+            />
             <CardTitle>Create an account</CardTitle>
-            <CardDescription>Please enter your credentials to create an account.</CardDescription>
+            <CardDescription>
+              Please enter your credentials to create an account.
+            </CardDescription>
           </CardHeader>
           <CardContent>
-            
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="username">Username</Label>
-                  <Input id="username" required name="username" placeholder="Username" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="firstname">First name</Label>
-                  <Input id="firstname" required name="firstname" placeholder="First name" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="lastname">First name</Label>
-                  <Input id="lastname" required name="lastname" placeholder="Last name" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="password">Password</Label>
-                  <Input id="password" required name="password" type="password" placeholder="Password" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="confirm-password">Confirm Password</Label>
-                  <Input id="confirm-password" required name="confirmPassword" type="password" placeholder="Confirm Password" />
-                </div>
-
-                {formState.errors && (
-                  Object.keys(formState.errors).map((error) => (
-                    <p key={error} className="text-red-500 font-medium">{formState.errors[error]}</p>
-                  ))
-                )}
-
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  required
+                  name="username"
+                  placeholder="Username"
+                />
               </div>
-            
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="firstname">First name</Label>
+                <Input
+                  id="firstname"
+                  required
+                  name="firstname"
+                  placeholder="First name"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="lastname">First name</Label>
+                <Input
+                  id="lastname"
+                  required
+                  name="lastname"
+                  placeholder="Last name"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  required
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="confirm-password">Confirm Password</Label>
+                <Input
+                  id="confirm-password"
+                  required
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                />
+              </div>
+
+              {formState.errors &&
+                Object.keys(formState.errors).map((error) => (
+                  <p key={error} className="text-red-500 font-medium">
+                    {formState.errors[error]}
+                  </p>
+                ))}
+            </div>
           </CardContent>
           <CardFooter className="flex flex-col items-center space-y-2">
             <Button type="submit">Register</Button>
@@ -74,4 +114,3 @@ export default function RegisterPage() {
     </div>
   );
 }
-
