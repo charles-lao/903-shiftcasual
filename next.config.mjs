5ecdab69d180a9155563e6566966627952837b1e
Charles Lao <charles.h.lao@gmail.com>
2024-11-06 15:28:35 +1300

    enable caching

--- next.config.mjs
+++ next.config.mjs
@@ -7,43 +7,57 @@ const nextConfig = {
 
   compress: false,
 
-  // // Disable JS/TS minification
-  // swcMinify: false, // Disables JavaScript/TypeScript minification
+  // Disable JS/TS minification
+  swcMinify: false, // Disables JavaScript/TypeScript minification
 
-  // // Disable CSS minification (Experimental)
-  // experimental: {
-  //   optimizeCss: false, // Disable CSS minification
-  // },
+  // Disable CSS minification (Experimental)
+  experimental: {
+    optimizeCss: false, // Disable CSS minification
+  },
 
   // Disable Next.js image optimization
   images: {
     unoptimized: true, // Disable image optimization
   },
 
-  // Disable cache headers for static assets
+  // // Disable cache headers for static assets
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)", // Applies to all routes
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value:
+  //             "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
+
   async headers() {
     return [
       {
-        source: "/(.*)", // Applies to all routes
+        source: "/(.*)", // Apply to all routes
         headers: [
           {
             key: "Cache-Control",
-            value:
-              "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+            value: "public, max-age=31536000, immutable", // Example for static caching
           },
         ],
       },
     ];
   },
 
-  // // Custom Webpack configuration to disable minification
-  // webpack: (config, { isServer }) => {
-  //   // Disable minification in production
-  //   if (!isServer) {
-  //     config.optimization.minimizer = [];
-  //   }
-  //   return config;
-  // },
+  // Custom Webpack configuration to disable minification
+  webpack: (config, { isServer }) => {
+    // Disable minification in production
+    if (!isServer) {
+      config.optimization.minimizer = [];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
